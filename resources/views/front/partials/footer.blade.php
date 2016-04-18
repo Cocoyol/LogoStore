1bc47fd21d189eee3f0a169a0a942d8435cc3219
Dave <davebeatledoor@gmail.com>
2016-04-18 18:02:04 -0500

    Proyecto gestor de venta de logotipos para empresas
    Desarollo de template con bootstrap

---
+++ resources/views/front/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
+                <p class="text-muted">TODOS LOS DERECHOS RESERVADOS</p>
+        </div>
+    </div>
+</footer>
