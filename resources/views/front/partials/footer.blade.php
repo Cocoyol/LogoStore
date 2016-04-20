b146dc118c21c3df779432017fc9dd02166d415b
Dave <davebeatledoor@gmail.com>
2016-04-20 15:47:29 -0500

    Finalización de template inicio y impresión dimanica en el home

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,7 +1,23 @@
 <footer class="footer">
     <div class="container">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
-                <p class="text-muted">TODOS LOS DERECHOS RESERVADOS</p>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="row">
+                <div class="col-xs-12 col-sm-3 col-sm-offset-2 col-md-3 col-md-offset-2">
+                    <p class="text-muted center-block">TODOS LOS DERECHOS RESERVADOS</p>
+                </div>
+
+                <div class="col-xs-12 col-sm-1 col-md-1">
+                    <p class="text-muted center-block">CDMX</p>
+                </div>
+
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <p class="text-muted center-block">TÉRMINOS Y CONDICIONES</p>
+                </div>
+
+                <div class="col-xs-12 col-sm-1 col-md-1">
+                    <div class="text-muted-img">{{ Html::image('assets/images/facebook_icon.png', 'facebook' ,['class' => 'image-responsive center-block']) }}</div>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
